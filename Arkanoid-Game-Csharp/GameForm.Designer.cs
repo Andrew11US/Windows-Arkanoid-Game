5491606ab7f356d7680e368597955bbec04e95b4
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-04-20 22:50:09 +0200

    Exported Full game logic

--- Arkanoid-Game-Csharp/GameForm.Designer.cs
+++ Arkanoid-Game-Csharp/GameForm.Designer.cs
@@ -29,19 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(209, 79);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // timer
             // 
@@ -52,9 +41,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1352, 1015);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(697, 477);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
             this.Name = "GameForm";
             this.Text = "Arkanoid by Andrii Halabuda";
@@ -65,7 +54,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer;
     }
 }
--- Arkanoid-Game-Csharp/GameForm.cs
+++ Arkanoid-Game-Csharp/GameForm.cs
@@ -32,6 +32,7 @@ namespace Arkanoid_Game_Csharp
         public GameForm()
         {
             InitializeComponent();
+            ClientSize = new Size(Const.WINDOW_WIDTH, Const.WINDOW_HEIGHT);
             resetScene(true);
             KeyPreview = true;
 
@@ -76,73 +77,150 @@ namespace Arkanoid_Game_Csharp
 
         private void GameForm_Paint(object sender, PaintEventArgs e)
         {
-            // MARK: Constantly updates ball position in certain direction and speed
-            ball.x += ball.xDirection * ball.speed;
-            ball.y += ball.yDirection * ball.speed;
-
             e.Graphics.FillEllipse(Brushes.Red, ball.ToRectangle());
             e.Graphics.FillRectangle(Brushes.Red, paddle.ToRectangle());
-            bricks.ForEach(brick =>
+            //if (ball.xDirection == 0 || ball.yDirection == 0)
+            //{
+
+            //    ball.xDirection = -1;
+            //    ball.yDirection = -1;
+            //}
+
+            // MARK: Ball hits left or right edge
+            if (ball.x < 0 || ball.x + ball.width > Const.WINDOW_WIDTH)
             {
+                ball.xDirection *= -1;
+            }
+
+            // MARK: Ball hits a top edge
+            if (ball.y < 0)
+            {
+                ball.yDirection *= -1;
+            }
+
+            // MARK: Ball hits a paddle on top of the platform or on one of the sides
+            if (ball.ToRectangle().IntersectsWith(paddle.ToRectangle()))
+            {
+                if (ball.x + ball.size < paddle.x + 10)
+                {
+                    ball.xDirection *= -1;
+                    //System.out.println("Left side intersection");
+                }
+                else if (ball.x > paddle.x + paddle.width - 10)
+                {
+                    ball.xDirection *= -1;
+                    //System.out.println("Right side intersection");
+                }
+
+                ball.yDirection *= -1;
+            }
+
+            // MARK: Ball crosses bottom edge
+            if (ball.y > Const.WINDOW_WIDTH)
+            {
+                lives -= 1;
+                if (lives != 0)
+                {
+                    resetScene(false);
+                }
+                else
+                {
+                    
+                    
+                    //  call gameOver method
+                    
+                }
+            }
+
+            // MARK: Ball hits one or multiple blocks, handling collision
+            bricks.ForEach(brick => {
                 if (!brick.destroyed)
                 {
                     e.Graphics.FillRectangle(Brushes.BlueViolet, brick.ToRectangle());
                 }
 
-                if (brick.ToRectangle().IntersectsWith(ball.ToRectangle()))
+                if (!brick.destroyed && ball.ToRectangle().IntersectsWith(brick.ToRectangle()))
                 {
                     brick.destroyed = true;
+                    if (ball.x + ball.size < brick.x + 5)
+                    {
+                        ball.xDirection *= -1;
+                        //System.out.println("Left side hit");
+                    }
+                    else if (ball.x > brick.x + brick.width - 5)
+                    {
+                        ball.xDirection *= -1;
+                        //System.out.println("Right side hit");
+                    }
+                    else if (ball.y + ball.size < brick.y + 5)
+                    {
+                        ball.yDirection *= -1;
+                        
+                    }
+                    else if (ball.y > brick.y + brick.height - 5)
+                    {
+                        ball.yDirection *= -1;
+                        
+                    }
+                    else
+                    {
+                        ball.xDirection *= -1;
+                        ball.yDirection *= -1;
+                    }
+                    levelScore++;
+                    score++;
+                  
                 }
-
             });
-            //Rectangle r1 = new Rectangle(new Point(x, y), new Size(50, 50));
-            //Rectangle r2 = new Rectangle(new Point(100, 10), new Size(50, 50));
-            //if (r1.IntersectsWith(r2))
-            //{
-            //    Console.WriteLine("Intersection");
-            //    r2.Y = 200;
-            //}
-            //e.Graphics.FillRectangle(new SolidBrush(Color.Green), r);
-            //e.Graphics.FillEllipse(Brushes.Black, r1);
-            //e.Graphics.FillRectangle(Brushes.Black, r2);
+
+            if (levelScore == bricksCount)
+            {
+       
+                level += 1;
+                levelScore = 0;
+
+                if (lives != 5)
+                {
+                    lives += 1;
+                }
+     
+                resetScene(true);
+            }
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
+            // MARK: Constantly updates ball position in certain direction and speed
+            ball.x += ball.xDirection * ball.speed;
+            ball.y += ball.yDirection * ball.speed;
             Invalidate();
-        }
-
-        private void GameForm_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            //if (e. == Keys.Right)
-            //{
-
-            //}
-        }
-
-        private void GameForm_KeyUp(object sender, KeyEventArgs e)
-        {
-
         }
 
         private void GameForm_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyData == Keys.D)
+            // MARK: Start game or pause
+            if (e.KeyData == Keys.Space)
             {
-                paddle.x += 20;
-                Invalidate();
+                timer.Enabled = !timer.Enabled;
             }
-            else if (e.KeyData == Keys.A)
+            // MARK: Paddle left direction
+            if (e.KeyData == Keys.A && paddle.x > 0)
             {
-                paddle.x -= 20;
-                Invalidate();
+                if (timer.Enabled)
+                {
+                    paddle.x -= 20;
+                    Invalidate();
+                }
             }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            timer.Enabled = !timer.Enabled;
-            //Invalidate();
+            // MARK: Paddle right direction
+            if (e.KeyData == Keys.D && paddle.x + paddle.width < Const.WINDOW_WIDTH)
+            {
+                if (timer.Enabled)
+                {
+                    paddle.x += 20;
+                    Invalidate();
+                }
+            }
         }
     }
         
